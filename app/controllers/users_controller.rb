2500adc9512762055df1736ee2b4c02894efe1ba
steefg3000 <steef.gijsbers@gmail.com>
2022-07-08 16:57:01 +0200

    Add login functionality to users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,9 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update]
+  before_action :require_user, only: [:edit, :update]
+  before_action :require_same_user, only: [:edit, :update]
 
   def show
-    @user = User.find(params[:id])
     @articles = @user.articles.paginate(page: params[:page], per_page: 5)
   end
 
@@ -14,11 +16,9 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
   end
 
   def update
-    @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:notice] = "Your account information was successfully updated."
       redirect_to @user
@@ -31,6 +31,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       flash[:notice] = "Welcome to the Alpha Blog #{@user.username}, you have successfully signed up."
+      session[:user_id] = @user.id
       redirect_to @user
     else
       render 'new'
@@ -40,7 +41,18 @@ class UsersController < ApplicationController
 
   private
 
+  def set_user
+    @user = User.find(params[:id])
+  end
+
   def user_params
     params.require(:user).permit(:username, :email, :password)
+  end
+
+  def require_same_user
+    if @user != current_user
+      flash[:alert] = "You can only edit your own account"
+      redirect_to @user
+    end
   end
 end
